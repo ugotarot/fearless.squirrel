ad3350b75c8ce5ca896ca7a81c19d834d8ba8bf6
ugo.tarot <ugo.tarot@epita.fr>
2022-09-28 20:57:27 +0200

    [ADD] ennemy object

---
+++ game/ennemy.js
@@ -0,0 +1,75 @@
+var Ennemy = function(position, direction) {
+    this.position = position;
+    this.life = 3;
+    this.bullets = new Array();
+    this.direction = direction;
+    this.speed = 0;
+
+    this.material = new THREE.MeshLambertMaterial({
+        color: 0xFF00FF,
+        });
+
+    var singleGeometry = new THREE.Geometry();
+
+    vehiculeMesh = new THREE.ConeGeometry(5, 20, 32);
+    this.graphic = new THREE.Mesh(vehiculeMesh, this.material);
+    this.graphic.position.z = 6;
+
+    this.graphic.rotateOnAxis(new THREE.Vector3(0,0,1), this.direction+(3*Math.PI/2));
+};
+
+Ennemy.prototype.accelerate = function (distance) {
+    var max = 2;
+
+    this.speed += distance / 4;
+    if (this.speed >= max) {
+        this.speed = max;
+    }
+};
+
+Ennemy.prototype.decelerate = function (distance) {
+    var min = -1;
+
+    this.speed -= distance / 16;
+    if (this.speed <= min) {
+        this.speed = min;
+    }
+};
+
+Ennemy.prototype.displayInfo = function () {
+    jQuery('#'+this.name+' >.life').text(this.life);
+}
+
+Ennemy.prototype.turnRight = function (angle) {
+    this.direction -= angle;
+    this.graphic.rotateOnAxis(new THREE.Vector3(0,0,1), -angle);
+};
+
+Ennemy.prototype.turnLeft = function (angle) {
+    this.direction += angle;
+    this.graphic.rotateOnAxis(new THREE.Vector3(0,0,1), angle);
+};
+
+Ennemy.prototype.move = function () {
+    var moveTo = new THREE.Vector3(
+        this.speed * Math.cos(this.direction) + this.position.x,
+        this.speed * Math.sin(this.direction) + this.position.y,
+        this.graphic.position.z
+    );
+
+    this.position = moveTo;
+
+    if (this.speed > 0) {
+        this.speed = this.speed - 0.04;
+    }
+    else if (this.speed < 0) {
+        this.speed = this.speed + 0.04;
+    }
+
+    this.graphic.position.x = this.position.x;
+    this.graphic.position.y = this.position.y;
+    
+    //light1.position.x = this.position.x;
+    //light1.position.y = this.position.y;
+   //light1.position.z = this.graphic.position.z + 500;
+};
